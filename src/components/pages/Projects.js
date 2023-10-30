ae1c1d6c8014278ce7eee5a52375f7a9c254f66a
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2023-10-30 16:46:46 -0400

    added project sectioning

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -7,23 +7,44 @@ export default function Projects() {
       <h1 className="text-center p-3 home-head">Projects</h1>
       <div className="grid-container">
         <div
-          class="about-container standard-container animate__slideInRight animate__animated"
-          id="about"
+          className="about-container standard-container animate__slideInRight animate__animated"
+          id=""
         >
-          <div class="header-content">
-            <h3> About </h3>
-            <p class="writing-format">
-              My name is Keyshawn Bhagwandin. I am a 25 year old young
-              professional Full Stack developer currently residing in the GTA. I
-              am very passionate about coding and web development. I love
-              developing functional projects, responsive web applications, and
-              practical software’s. Some of the technologies I have become very
-              skilled in include HTML, CSS and JavaScript. I am dedicated to my
-              craft and work to keep build on my already high level skills in
-              programming and coding languages.
+          <div className="header-content">
+            <h3> Project1 </h3>
+            <p className="writing-format">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
             </p>
           </div>
-          <div class="about-content"></div>
+          <div className="about-content"></div>
+        </div>
+        <div
+          className="projects-container flip-container animate__slideInLeft animate__animated"
+          id=""
+        >
+          <div className="header-content flipped-header">
+            <h3> Project2 </h3>
+          </div>
+          <p className="writing-format">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
       </div>
     </>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,7 @@
 export default function Resume() {
   return (
     <div className="about-main">
-      Resume
+      Services
     </div>
   );
 }
